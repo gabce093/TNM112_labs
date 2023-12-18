fd6b1a34f341562aea14c56430fddedfafab5a2c
gabce093 <gc.yppoo@gmail.com>
2023-12-18 21:07:35 +0100

    Snyggare

--- TNM112_Lab3/main.py
+++ TNM112_Lab3/main.py
@@ -32,51 +32,46 @@ def pre_processing_org(image):
     image /= 255.0
     return image
 
+def createData(path, batch_size, image_size):
+    datagen_cover = ImageDataGenerator(preprocessing_function=pre_processing)
 
+    covered_data = datagen_cover.flow_from_directory(
+        directory=path,
+        target_size=image_size,
+        batch_size=batch_size,
+        class_mode=None,
+        seed=None,
+        shuffle=False,
+    )
+
+    datagen_org = ImageDataGenerator(preprocessing_function=pre_processing_org)
+
+    org_data = datagen_org.flow_from_directory(
+        directory=path,
+        target_size=image_size,
+        batch_size=batch_size,
+        class_mode=None,
+        seed=None,
+        shuffle=False,
+    )
+    return zip(covered_data, org_data)
+
+epochs = 1
 image_size = (128, 128)
-
-datagen_cover = ImageDataGenerator(
-    preprocessing_function=pre_processing,
-    # validation_split=0.2  # Split for validation if needed
-)
-
-datagen_org = ImageDataGenerator(
-    preprocessing_function=pre_processing_org,
-
-)
-
-covered_data = datagen_cover.flow_from_directory(
-    directory='vegetable_images/train',
-    target_size=image_size,
-    batch_size=1,
-    class_mode=None,
-    seed=None,
-    shuffle=False,
-)
-
-org_data = datagen_org.flow_from_directory(
-    directory='vegetable_images/train',
-    target_size=image_size,
-    batch_size=1,
-    class_mode=None,
-    seed=None,
-    shuffle=False,
-)
-
-combined_data = zip(covered_data, org_data)
+combined_data = createData('vegetable_images/train', batch_size=1, image_size=image_size)
 
 model = Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', padding="same", input_shape=(image_size[0], image_size[1], 3)),
     layers.MaxPooling2D((2, 2), padding='same'),
     layers.Conv2D(64, (3, 3), activation='relu', padding="same"),
-    layers.MaxPooling2D((2, 2), padding='same'),
+    #layers.MaxPooling2D((2, 2), padding='same'),
     layers.Conv2D(128, (3, 3), activation='relu', padding="same"),
     layers.MaxPooling2D((2, 2), padding='same'),
 
     layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
     layers.UpSampling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-    layers.UpSampling2D((2, 2)),
+    #layers.UpSampling2D((2, 2)),
     layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
     layers.UpSampling2D((2, 2)),
     layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same'),
@@ -84,16 +79,14 @@ model = Sequential([
 
 model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
-steps_per_epoch = min(len(covered_data), len(org_data))
+steps_per_epoch = 15000
 
-#model.fit(combined_data, epochs=1, steps_per_epoch=steps_per_epoch)
-#model.save("128_org.keras")
+log = model.fit(combined_data, epochs=epochs, steps_per_epoch=steps_per_epoch)
+#model.save("test.keras")
 
 #model.evaluate(model, training_data[0])
 
-model = keras.models.load_model("128_org.keras")
-
-
+#model = keras.models.load_model("128_org.keras")
 
 #regenerated = model.predict(covered_data, steps=10)
 
@@ -103,10 +96,11 @@ plt.figure(figsize=(20, 4))
 for i in range(n):
     img_pair = next(combined_data)
     destroyed = img_pair[0]
+    original = img_pair[1]
     regenerated = model.predict(destroyed)[0]
     # Original image
     ax = plt.subplot(3, n, i + 1)
-    plt.imshow(org_data[i][0])
+    plt.imshow(original[0])
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
