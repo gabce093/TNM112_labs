cc7e9f75ac36d491a826b75e692903546ec965b8
gabce093 <gc.yppoo@gmail.com>
2023-12-19 14:42:16 +0100

    some models

--- TNM112_Lab3/main.py
+++ TNM112_Lab3/main.py
@@ -7,14 +7,42 @@ from keras.models import Sequential
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def plot_training(log):
+    N_train = len(log.history['loss'])
+    N_valid = len(log.history['val_loss'])
+
+    plt.figure(figsize=(18, 4))
+
+    # Plot loss on training and validation set
+    plt.subplot(1, 2, 1)
+    plt.plot(log.history['loss'])
+    plt.plot(np.linspace(0, N_train - 1, N_valid), log.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.grid('on')
+    plt.legend(['Train', 'Validation'])
+
+    # Plot accuracy on training and validation set
+    plt.subplot(1, 2, 2)
+    plt.plot(log.history['accuracy'])
+    plt.plot(np.linspace(0, N_train - 1, N_valid), log.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.grid('on')
+    plt.legend(['Train', 'Validation'])
+
+    plt.show()
 def random_cover(image):
     # Generate random coordinates for the top-left corner of the covered region
     top_left_x = np.random.randint(0, image.shape[1])
     top_left_y = np.random.randint(0, image.shape[0])
 
     # Generate random dimensions for the covered region
-    width = np.random.randint(10, 30)
-    height = np.random.randint(10, 30)
+    width = np.random.randint(30, 100)
+    height = np.random.randint(30, 100)
 
     # Set the pixels in the covered region to a constant value (e.g., zero for black)
     image[top_left_y:top_left_y + height, top_left_x:top_left_x + width, :] = 0
@@ -56,22 +84,23 @@ def createData(path, batch_size, image_size):
     )
     return zip(covered_data, org_data)
 
-epochs = 1
-image_size = (128, 128)
+epochs = 5
+image_size = (224, 224)
 combined_data = createData('vegetable_images/train', batch_size=1, image_size=image_size)
+val_data = createData('vegetable_images/validation', batch_size=1, image_size=image_size)
 
 model = Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', padding="same", input_shape=(image_size[0], image_size[1], 3)),
     layers.MaxPooling2D((2, 2), padding='same'),
     layers.Conv2D(64, (3, 3), activation='relu', padding="same"),
-    #layers.MaxPooling2D((2, 2), padding='same'),
+    layers.MaxPooling2D((2, 2), padding='same'),
     layers.Conv2D(128, (3, 3), activation='relu', padding="same"),
     layers.MaxPooling2D((2, 2), padding='same'),
 
     layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
     layers.UpSampling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-    #layers.UpSampling2D((2, 2)),
+    layers.UpSampling2D((2, 2)),
     layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
     layers.UpSampling2D((2, 2)),
     layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same'),
@@ -81,12 +110,14 @@ model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
 steps_per_epoch = 15000
 
-log = model.fit(combined_data, epochs=epochs, steps_per_epoch=steps_per_epoch)
-#model.save("test.keras")
+log = model.fit(combined_data, epochs=epochs, steps_per_epoch=steps_per_epoch,)
+                #validation_data=val_data, batch_size=1,validation_freq=1, verbose=True)
+model.save("224_org_5ep.keras")
+#plot_training(log)
 
 #model.evaluate(model, training_data[0])
 
-#model = keras.models.load_model("128_org.keras")
+#model = keras.models.load_model("128_delPool_5ep.keras")
 
 #regenerated = model.predict(covered_data, steps=10)
 
